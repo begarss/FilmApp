ab3f04f9d7e06ce362634178139986ae91e603a2
begarss <44815519+begarss@users.noreply.github.com>
2020-04-28 19:22:40 +0600

    Revert "Crashlytics"

--- app/src/main/java/com/example/themovie/view/fragment/UserFragment.kt
+++ app/src/main/java/com/example/themovie/view/fragment/UserFragment.kt
@@ -14,8 +14,6 @@ import com.example.themovie.model.authorization.LoginSharedPref
 
 class UserFragment : Fragment() {
     private lateinit var logOut:Button
-    private lateinit var crash:Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,17 +27,14 @@ class UserFragment : Fragment() {
         val view: View = LayoutInflater.from(container?.context)
             .inflate(R.layout.fragment_user, container, false)
         logOut = view?.findViewById(R.id.logOut)!!
-        crash = view?.findViewById(R.id.crash)!!
-
         logOut.setOnClickListener(View.OnClickListener {
             LoginSharedPref().clearUserName(requireActivity())
             val intent = Intent(this.context, LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+
             startActivity(intent)
         })
-        crash.setOnClickListener(View.OnClickListener {
-            throw RuntimeException("App crashed for test")
-        })
+
         return view
     }
 }
